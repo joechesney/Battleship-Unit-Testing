286488bab154417c2b592973944f299ce0a8f748
joechesney <33613119+joechesney@users.noreply.github.com>
2018-03-31 11:57:14 -0500

    refactored ship_methods

--- game_logic/ship_methods.js
+++ game_logic/ship_methods.js
@@ -1,13 +1,10 @@
-function checkForShip (player, coordinates) {
-	var shipPresent, ship;
-	
-	for (var i = 0; i < player.ships.length; i++) {
+module.exports.checkForShip = (player, coordinates) =>{
+	let shipPresent, ship;
+	for (let i = 0; i < player.ships.length; i++) {
 		ship = player.ships[i];
-		
 		shipPresent = ship.locations.filter(function (actualCoordinate) {
 			return (actualCoordinate[0] === coordinates[0]) && (actualCoordinate[1] === coordinates[1]);
 		})[0];
-		
 		if (shipPresent) {
 			return ship;
 		} 
@@ -15,17 +12,13 @@ function checkForShip (player, coordinates) {
 	return false;
 }
 
-function damageShip (ship, coordinates) {
+module.exports.damageShip = (ship, coordinates) => {
 	ship.damage.push(coordinates);
 }
 
-function fire(player, coordinates) {
-	var ship = checkForShip(player, coordinates);
+module.exports.fire = (player, coordinates) => {
+	let ship = checkForShip(player, coordinates);
 	if (ship) {
 		damageShip(ship, coordinates);
 	}	
 }
-
-module.exports.checkForShip = checkForShip;
-module.exports.damageShip = damageShip;
-module.exports.fire = fire;
